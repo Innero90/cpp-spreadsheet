7ac80297312c0bf821a05cd3a82f343389ebcf75
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-09-14 17:51:13 +0300

    Update common.h

--- spreadsheet/common.h
+++ spreadsheet/common.h
@@ -9,7 +9,8 @@
 #include <vector>
 
 // Позиция ячейки. Индексация с нуля.
-struct Position {
+struct Position
+{
     int row = 0;
     int col = 0;
 
@@ -26,7 +27,8 @@ struct Position {
     static const Position NONE;
 };
 
-struct Size {
+struct Size
+{
     int rows = 0;
     int cols = 0;
 
@@ -34,21 +36,50 @@ struct Size {
 };
 
 // Описывает ошибки, которые могут возникнуть при вычислении формулы.
-class FormulaError {
+class FormulaError
+{
 public:
-    enum class Category {
+    enum class Category
+    {
         Ref,    // ссылка на ячейку с некорректной позицией
         Value,  // ячейка не может быть трактована как число
-        Div0,  // в результате вычисления возникло деление на ноль
+        Div0,   // в результате вычисления возникло деление на ноль
     };
 
-    FormulaError(Category category);
+    FormulaError(Category category)
+        : category_(category)
+    {};
 
-    Category GetCategory() const;
+    Category GetCategory() const
+    {
+        return category_;
+    }
 
-    bool operator==(FormulaError rhs) const;
+    bool operator==(FormulaError rhs) const
+    {
+        return category_ == rhs.category_;
+    }
 
-    std::string_view ToString() const;
+    std::string_view ToString() const
+    {
+        using namespace std::string_view_literals;
+
+        switch (category_)
+        {
+        case FormulaError::Category::Ref:
+            return { "#REF!"sv };
+            break;
+        case FormulaError::Category::Value:
+            return { "#VALUE!"sv };
+            break;
+        case FormulaError::Category::Div0:
+            return { "#DIV/0!"sv };
+            break;
+        default:
+            return { ""sv };
+            break;
+        }
+    }
 
 private:
     Category category_;
@@ -57,26 +88,30 @@ private:
 std::ostream& operator<<(std::ostream& output, FormulaError fe);
 
 // Исключение, выбрасываемое при попытке передать в метод некорректную позицию
-class InvalidPositionException : public std::out_of_range {
+class InvalidPositionException : public std::out_of_range
+{
 public:
     using std::out_of_range::out_of_range;
 };
 
 // Исключение, выбрасываемое при попытке задать синтаксически некорректную
 // формулу
-class FormulaException : public std::runtime_error {
+class FormulaException : public std::runtime_error
+{
 public:
     using std::runtime_error::runtime_error;
 };
 
 // Исключение, выбрасываемое при попытке задать формулу, которая приводит к
 // циклической зависимости между ячейками
-class CircularDependencyException : public std::runtime_error {
+class CircularDependencyException : public std::runtime_error
+{
 public:
     using std::runtime_error::runtime_error;
 };
 
-class CellInterface {
+class CellInterface
+{
 public:
     // Либо текст ячейки, либо значение формулы, либо сообщение об ошибке из
     // формулы
@@ -103,7 +138,8 @@ inline constexpr char FORMULA_SIGN = '=';
 inline constexpr char ESCAPE_SIGN = '\'';
 
 // Интерфейс таблицы
-class SheetInterface {
+class SheetInterface
+{
 public:
     virtual ~SheetInterface() = default;
 
